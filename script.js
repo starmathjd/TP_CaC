95bf3f67342605745450680669755c46e4c864dc
starmathjd <roblesdavidr@gmail.com>
2023-10-02 11:42:19 -0300

    Se agrega el TP

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/******************************************************************************/
+/********************** DESPLAZAMIENTO DEL MENÚ SUPERIOR **********************/
+/******************************************************************************/ 
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+
+}
+
+
+
+/******************************************************************************/
+/****************** MANTENER LA BARRA SUPERIOR AL SCROLLEAR *******************/
+/******************************************************************************/ 
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+
+/******************************************************************************/
+/****************** BOTON PARA SUBIR AL INICIO DE LA PÁGINA *******************/
+/******************************************************************************/
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
