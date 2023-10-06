252c5a686618038dc0dbfd03d432362d3ed7eeda
starmathjd <142439744+starmathjd@users.noreply.github.com>
2023-10-06 01:48:20 -0300

    script.js

--- script.js
+++ script.js
@@ -50,3 +50,31 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
+
+
+
+/******************************************************************************/
+/********************************* LOGIN *********************************/
+/******************************************************************************/
+
+
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+function register() {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
